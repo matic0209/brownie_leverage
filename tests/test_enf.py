4eec39cd45bc1563a6dd7bbe723280ed1e7cdb37
jinbo zhang <cszhangjinbo@gmail.com>
2022-05-05 22:34:15 +0800

    u

--- tests/test_enf.py
+++ tests/test_enf.py
@@ -68,3 +68,9 @@ def test_can_get_latest_price(
 
     balance_after = account.balance()
     log("balance of account eth end", str(balance_after))
+
+    tx = deploy_vault.earnReward(
+        {"from": account, "gas_price": 100, "gas_limit": 3000000, "allow_revert": True},
+    )
+
+    print(tx.info())
