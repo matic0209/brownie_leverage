23022d10c9ed6754d8a4b6b362c6823e683f33ad
jinbo zhang <cszhangjinbo@gmail.com>
2022-05-25 21:39:02 +0800

    Update

--- contracts/core/EFLeverVault.sol
+++ contracts/core/EFLeverVault.sol
@@ -127,7 +127,9 @@ contract EFLeverVault is Ownable, ReentrancyGuard{
   function _deposit(uint256 amount, uint256 fee_amount) internal{
     IWETH(weth).withdraw(amount);
 
-    ILido(lido).submit.value(address(this).balance)(address(this));
+    
+
+    ICurve(curve_pool).exchange(0, 1, address(this).balance, 0);
     
     uint256 lido_bal = IERC20(lido).balanceOf(address(this));
     if (IERC20(lido).allowance(address(this), aave) != 0) {IERC20(lido).safeApprove(aave, 0);}
