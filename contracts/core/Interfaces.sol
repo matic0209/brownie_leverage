8616e57e62efe38466b6f140729ba5b4080e478f
jinbo zhang <cszhangjinbo@gmail.com>
2022-06-06 18:35:39 +0800

    update

--- contracts/core/Interfaces.sol
+++ contracts/core/Interfaces.sol
@@ -7,11 +7,6 @@ contract CurveInterface256{
     function exchange(uint256 i, uint256 j, uint256 dx, uint256 min_dy) external payable returns(uint256);//change i to j
     //0 weth, 1 crv
 }
-
-contract CurveInterface2561{
-    function exchange(uint256 i, uint256 j, uint256 dx, uint256 min_dy, bool use_eth) external payable returns(uint256);//change i to j
-    //0 weth, 1 crv
-}
 contract CurveInterface128{
     function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external returns(uint256);
     function get_dy(int128 i, int128 j, uint256 dx) external view returns(uint256);
@@ -29,4 +24,3 @@ contract ConvexInterface{
 contract ChainlinkInterface{
   function latestAnswer() external view returns (int256);
 }
-
